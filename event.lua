9e5da3d208691340c167885db3498d44d1794d20
BaoBao-creator <ba0ba0113377@gmail.com>
2025-08-26 13:54:03 +0700

    Update event.lua

--- event.lua
+++ event.lua
@@ -1,1 +1,29 @@
 
+local simpleui = loadstring(game:HttpGet("https://raw.githubusercontent.com/BaoBao-creator/Simple-Ui/main/ui.lua"))()
+local window = simpleui:CreateWindow({Name= "Simple Hub - Event, BaoBao developer"})
+local eventtab = window:CreateTab("Event Tab")
+eventtab:CreateToggle({
+    Name = "Auto feed to beanstalk",
+    Callback = function(v)
+        if v then
+            autofeed()
+        else
+            feeding = false
+        end
+    end
+})
+eventtab:CreateButton({
+    Name = "Auto collect reward points",
+    Callback = function()
+        for i = 1, 20 do
+            ReplicatedStorage.GameEvents.BeanstalkREClaimReward:FireServer(i)
+        end
+    end
+})
+eventtab:CreateButton({
+    Name = "Open/close event shop",
+    Callback = function()
+        local gui = player.PlayerGui:WaitForChild("EventShop_UI")
+        gui.Enabled = not gui.Enabled
+    end
+})

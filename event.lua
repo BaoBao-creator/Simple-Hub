6c8a330581abfaec911614680b86edc39a0d692a
BaoBao-creator <ba0ba0113377@gmail.com>
2025-08-26 13:55:57 +0700

    Update event.lua

--- event.lua
+++ event.lua
@@ -1,4 +1,19 @@
-
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local LocalPlayer = Players.LocalPlayer
+local character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
+local humanoid = character:WaitForChild("Humanoid")
+local CollectList = {}
+local collecting = false
+local feeding = false
+local mainfarm = workspace.Farm
+local userfarm
+for _, farm in ipairs(mainfarm:GetChildren()) do
+    if farm.Important.Data.Owner.Value == LocalPlayer.Name then
+        userfarm = farm
+        break
+    end
+end
 local simpleui = loadstring(game:HttpGet("https://raw.githubusercontent.com/BaoBao-creator/Simple-Ui/main/ui.lua"))()
 local window = simpleui:CreateWindow({Name= "Simple Hub - Event, BaoBao developer"})
 local eventtab = window:CreateTab("Event Tab")
@@ -23,7 +38,7 @@ eventtab:CreateButton({
 eventtab:CreateButton({
     Name = "Open/close event shop",
     Callback = function()
-        local gui = player.PlayerGui:WaitForChild("EventShop_UI")
+        local gui = LocalPlayerp.PlayerGui:WaitForChild("EventShop_UI")
         gui.Enabled = not gui.Enabled
     end
 })

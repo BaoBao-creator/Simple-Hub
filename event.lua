9adbfb383b87f7871d7462c0480d78047c172c00
BaoBao-creator <ba0ba0113377@gmail.com>
2025-08-27 10:07:20 +0700

    Update event.lua

--- event.lua
+++ event.lua
@@ -2,7 +2,7 @@ local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local feeding = false
-local eventshop = {"All", "Sprout Seed Pack", "Sprout Egg", "Mandrake Seed", "Sprout Crate", "Silver Fertilizer", "Canary Melon Seed", "Amberheart", "Spriggan", "Skyroot Chest", "Can Of Beans", "Griffin Statue", "Bouncy Mushroom", "}
+local eventshop = {"All", "Sprout Seed Pack", "Sprout Egg", "Mandrake Seed", "Sprout Crate", "Silver Fertilizer", "Canary Melon Seed", "Amberheart", "Spriggan", "Skyroot Chest", "Can Of Beans", "Griffin Statue", "Bouncy Mushroom", "Glowpod", "Flare Melon", "Pet Mutation Shard Giantbean", "Gnome"}
 local function autofeed()
     feeding = true
     coroutine.wrap(function()

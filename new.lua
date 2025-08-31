c5d629b7e9d7ecabd86ee11c8fa99be600ca1965
BaoBao-creator <ba0ba0113377@gmail.com>
2025-08-31 09:45:06 +0700

    Update new.lua

--- new.lua
+++ new.lua
@@ -1,1 +1,13 @@
-
+-- Roblox Services
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local VirtualUser = game:GetService("VirtualUser")
+local UserInputService = game:GetService("UserInputService")
+local RunService = game:GetService("RunService")
+-- Roblox Data
+local LocalPlayer = Players.LocalPlayer
+local character = LocalPlayer.Character
+local humanoid = character.Humanoid
+local humanoidRootPart = character.HumanoidRootPart
+-- Game List
+local CollectList = {}

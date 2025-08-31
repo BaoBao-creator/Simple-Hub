910e7f55d67eae0788d53c7f3472de5562743c26
BaoBao-creator <ba0ba0113377@gmail.com>
2025-08-31 09:52:30 +0700

    Update new.lua

--- new.lua
+++ new.lua
@@ -11,3 +11,17 @@ local humanoid = character.Humanoid
 local humanoidRootPart = character.HumanoidRootPart
 -- Game List
 local CollectList = {}
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = Rayfield:CreateWindow({
+   Name = "Simple Hub,
+   LoadingTitle = "Welcome!",
+   LoadingSubtitle = "by BaoBao",
+   ShowText = "Show UI",
+   Theme = "Bloom",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil,
+      FileName = "Big Hub"
+   }
+})
+local Tab = Window:CreateTab("Farm", 0)

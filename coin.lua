c33c147017f969db4ea108eabae035cf7ca29846
BaoBao-creator <ba0ba0113377@gmail.com>
2025-09-13 21:09:36 +0700

    Update coin.lua

--- coin.lua
+++ coin.lua
@@ -1,4 +1,12 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local coining = false
+local coindelay = 0.01
 local function autocoin()
-    ReplicatedStorage.Events.ClickMoney:FireServer()
+    coining = not coining
+    task.spawn(function()
+        while coining do
+            ReplicatedStorage.Events.ClickMoney:FireServer()
+            task.wait(coindelay)
+        end
+    end)
 end

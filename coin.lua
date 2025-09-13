629371a2a79ea6d131091c6ebffc810bec547dbe
BaoBao-creator <ba0ba0113377@gmail.com>
2025-09-13 21:12:26 +0700

    Update coin.lua

--- coin.lua
+++ coin.lua
@@ -10,3 +10,34 @@ local function autocoin()
         end
     end)
 end
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = Rayfield:CreateWindow({
+    Name = "Simple Hub",
+    LoadingTitle = "Welcome!",
+    LoadingSubtitle = "by BaoBao",
+    ShowText = "UI",
+    Theme = "Bloom",
+    ConfigurationSaving = {
+        Enabled = true,
+        FolderName = nil,
+        FileName = "Simple Hub Config"
+    }
+})
+local MainTab = Window:CreateTab("Main", 0)
+MainTab:CreateToggle({
+    Name = "Auto Coin",
+    CurrentValue = false,
+    Flag = "autohealToggle",
+    Callback = autoheal
+})
+
+MainTab:CreateSlider({
+    Name = "Auto Coin Delay",
+    Range = {0.01, 0.1},
+    Increment = 0.01,
+    CurrentValue = 0.01,
+    Flag = "DelaySlider",
+    Callback = function(v)
+        coindelay = v
+    end
+})
